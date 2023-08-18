b4c87294c3cc9c8d695e694a566d7f0fb5f88e77
Dominik <dominik.zemlik.dev@gmail.com>
2023-08-18 18:38:53 +0200

    Import fonts and create color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./dist/*.{html,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primaryBlack: 'var(--primaryBlack)',
+				primaryGreen: 'var(--primaryGreen)',
+				primaryGrey: 'var(--primaryGrey)',
+				primaryBeige: 'var(--primaryBeige)',
+				primaryWhite: 'var(--primaryWhite)',
+			},
+			fontFamily: {
+				montserrat: 'var(--montserrat)',
+				inter: 'var(--inter)',
+			},
+		},
 	},
 	plugins: [],
 };
