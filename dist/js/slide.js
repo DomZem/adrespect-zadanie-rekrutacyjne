07442cfc89dda6ffd705f4f65392bb1b69c30b37
Dominik <dominik.zemlik.dev@gmail.com>
2023-08-19 13:21:07 +0200

    Create and style intro section

---
+++ dist/js/slide.js
@@ -0,0 +1,32 @@
+const previousSlideButton = document.querySelector('.previousSlideButton');
+const nextSlideButton = document.querySelector('.nextSlideButton');
+
+let slideIndex = 1;
+
+const showSlides = (n) => {
+	const slides = document.querySelectorAll('.slide');
+
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+
+	slides.forEach((slide) => (slide.style.display = 'none'));
+
+	slides[slideIndex - 1].style.display = 'flex';
+};
+
+// Next/previous controls
+const plusSlides = (n) => {
+	showSlides((slideIndex += n));
+};
+
+previousSlideButton.addEventListener('click', () => {
+	plusSlides(-1);
+});
+nextSlideButton.addEventListener('click', () => plusSlides(1));
+
+showSlides(slideIndex);
